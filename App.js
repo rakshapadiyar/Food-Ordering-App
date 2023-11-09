ecad6b3449779f4604ae58795945845f39e6b436
Raksha Padiyar <rakshapadiyar100@gmail.com>
2023-11-09 12:26:53 +0530

    set up starting files

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const head = <h1>Hi</h1>;
+
+root.render(head);
