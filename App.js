82f605b74e886889eca4866130085a6e4745a3a7
Raksha Padiyar <rakshapadiyar100@gmail.com>
2023-11-10 15:07:25 +0530

    manually added restaurant cards

--- App.js
+++ App.js
@@ -1,8 +1,68 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import logo from "./images/logo.jpeg";
+import meghanaFood from "./images/meghana-food.jpeg";
+
+const stylecard = {
+  backgroundColor: "#f5ede2",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={stylecard}>
+      {/* INline style
+    or stylr{{ backgroundColor: "#f5ede2"}} */}
+      <h3>Meghana Food</h3>
+      <img className="res-logo" alt="meghana food" src={meghanaFood} />
+      <h4>Asian, Chinese, Mexican</h4>
+      <h4>⭐⭐⭐⭐</h4>
+      <p>38 minutes</p>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const head = <h1>Hi</h1>;
-
-root.render(head);
+root.render(<AppLayout />);
