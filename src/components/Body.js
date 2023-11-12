3789e988f6dbe1ec1655afcb23563a9bd28d2b51
Raksha Padiyar <rakshapadiyar100@gmail.com>
2023-11-12 22:08:28 +0530

    used local state variable to re render using useState function

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,11 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
   let c = 0;
   let temp_list = restaurantList;
+  let [list, setList] = useState(temp_list);
   return (
     <div className="body">
       <div className="filter">
@@ -11,15 +13,16 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             console.log("button clicked");
-            temp_list = restaurantList.filter((res) => res.starRating >= 4);
-            console.log(temp_list);
+            list = restaurantList.filter((res) => res.starRating >= 4);
+            console.log(list);
+            setList(list);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {temp_list.map((restaurant) => (
+        {list.map((restaurant) => (
           <RestaurantCard key={c++} resObj={restaurant} />
         ))}
       </div>
